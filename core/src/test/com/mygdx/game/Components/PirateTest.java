070d29ff1e9245c5f5cfd3443c0e9bc8a57ccdfa
Abdullah <abdullah141299@gmail.com>
2022-04-10 16:35:35 +0100

    added sample test and circleci config file

---
+++ core/src/test/com/mygdx/game/Components/PirateTest.java
@@ -0,0 +1,13 @@
+package com.mygdx.game.Components;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PirateTest {
+    @Test
+    void simpleTest() {
+        Assertions.assertEquals(100, 100);
+    }
+}
